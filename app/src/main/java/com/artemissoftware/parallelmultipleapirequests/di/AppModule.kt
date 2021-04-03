53373c5780132483c97f05987def597ebe2878c3
gustavo <gustavo.isaac@gmail.com>
2021-04-03 09:02:38 +0100

    AppModule

---
+++ app/src/main/java/com/artemissoftware/parallelmultipleapirequests/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.artemissoftware.parallelmultipleapirequests.di
+
+import com.artemissoftware.parallelmultipleapirequests.util.DispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
+}
