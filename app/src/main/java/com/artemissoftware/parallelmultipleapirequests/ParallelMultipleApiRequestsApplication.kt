cbe4e78e849ca2a9a87ce46fff527ae71f2a0840
gustavo <gustavo.isaac@gmail.com>
2021-04-03 09:25:26 +0100

    ParallelMultipleApiRequestsApplication

---
+++ app/src/main/java/com/artemissoftware/parallelmultipleapirequests/ParallelMultipleApiRequestsApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.parallelmultipleapirequests
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ParallelMultipleApiRequestsApplication : Application()
